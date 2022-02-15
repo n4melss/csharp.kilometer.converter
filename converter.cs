b6e0481e3fbd4c07b4406a9b5bc1ad18c5f4e91d
Leon <private.n4meless@gmail.com>
2022-02-15 19:00:37 +0100

    base: Upload converter.cs

---
+++ converter.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please enter your amount of kilometers: ");
+
+        double kilometers = Convert.ToDouble(Console.ReadLine());
+        double miles = kilometers / 1.6;
+
+        Console.WriteLine(kilometers + " kilometers are the same as " + miles + " miles!");
+    }
+}
